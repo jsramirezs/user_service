49ee4107f4ce142a329806661fa687bd2b9553c2
juan <juan.ramirez93@udea.edu.co>
2019-08-17 17:04:33 -0500

    First Commit

---
+++ src/main/java/com/proy_int/user_service/model/User.java
@@ -0,0 +1,28 @@
+package com.proy_int.user_service.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String cedula;
+	private String nombres;
+}
